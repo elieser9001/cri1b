a551064eb82f1d6a63eba1c1d95d93b269ae3292
Elieser Reyes <platalunasol@gmail.com>
2023-12-11 16:20:46 -0500

    reemplazando validacion de subscripcion por validacion de membresia

--- app/lib/wordpressApi.py
+++ app/lib/wordpressApi.py
@@ -16,55 +16,33 @@ def get_user_data(email: str):
     
     return json_data
 
-def active_subscriptions(user_json_data):
-    try:
-        meta_data = user_json_data[0]["meta_data"]    
-        subscriptions_ids = list(filter(lambda item: item["key"] == "_wcs_subscription_ids_cache", meta_data))
+def get_user_membership(user_id):
+    user_membership = fetch_json(f"memberships/members?customer={user_id}")
 
-        subscriptions_info = []
-
-        if subscriptions_ids and subscriptions_ids[0]["value"]:
-            for id in subscriptions_ids[0]["value"]:
-                result = fetch_json(f"subscriptions/{id}")
-                
-                # if result["next_payment_date_gmt"]:
-                if result["status"] == "active":
-                    # subscriptions_info.append(result["next_payment_date_gmt"])
-
-                    subscriptions_info.append({
-                        "product_name": result["line_items"][0]["name"],
-                        "product_id": result["line_items"][0]["product_id"],
-                        "expire": result["next_payment_date_gmt"]
-                    })
-
-        return subscriptions_info
-    except:
-        return None
-    
+    return user_membership
 
 def get_cri1_license(email: str):
     try:
         user_json_data = get_user_data(email=email)
-        subscriptions = active_subscriptions(user_json_data=user_json_data)
+        user_json_data = user_json_data[0]
         
-        if user_json_data:
-            user_json_data = user_json_data[0]
-
-            for sub in subscriptions:
-                if "expire" in sub:
-                    exp_datetime = datetime.strptime(sub["expire"], "%Y-%m-%dT%H:%M:%S")
-                    current_datetime = datetime.now()
-
-                    if exp_datetime - current_datetime > timedelta(0):
-                        return {"user_first_name": user_json_data["first_name"], "user_last_name": user_json_data["last_name"], "company_name": user_json_data["billing"]["company"], "valid_license":  True}
-                    else:
-                        return {"user_first_name": user_json_data["first_name"], "user_last_name": user_json_data["last_name"], "company_name": user_json_data["billing"]["company"], "valid_license":  False}
-                else:
-                    return {"user_first_name": user_json_data["first_name"], "user_last_name": user_json_data["last_name"], "company_name": user_json_data["billing"]["company"], "valid_license":  False}
-
-            return {"user_first_name": user_json_data["first_name"], "user_last_name": user_json_data["last_name"], "company_name": user_json_data["billing"]["company"], "valid_license":  False}
+        user_membership = get_user_membership(user_json_data["id"])
+        user_membership = user_membership[0]
+        
+        if user_membership["status"] == "active":
+            return {
+                "user_first_name": user_json_data["first_name"],
+                "user_last_name": user_json_data["last_name"],
+                "company_name": user_json_data["billing"]["company"],
+                "valid_license":  True
+            }
         else:
-            return {"user_first_name": "", "user_last_name": "", "company_name": "", "valid_license":  False}
+            return {
+                "user_first_name": "",
+                "user_last_name": "",
+                "company_name": "",
+                "valid_license":  False
+            }
     except Exception as e:
         print(e)
         return {"user_first_name": "", "user_last_name": "", "company_name": "", "valid_license":  False}
