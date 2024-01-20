2ec44a2f55f8382f3ebc3b86d0fa6187f0467cad
Elieser Reyes <platalunasol@gmail.com>
2024-01-19 21:24:55 -0500

    nuevo implementacion graphql para el main user email

--- app/lib/wordpressApi.py
+++ app/lib/wordpressApi.py
@@ -11,6 +11,17 @@ def fetch_json(wc_endpoint):
 
     return r_json
 
+def fetch_graphql(query, variables):
+    url = "https://cri1.com/graphql"
+
+    response = requests.post(url, auth=("Developer", "Developer_2023$"), json={"query": query, "variables": variables})
+    try:
+        r_json = response.json()
+    except:
+        return None
+
+    return r_json
+
 def get_users_data():
     # json_data = fetch_json(f"customers?email={email}&role=all")
     json_data = fetch_json(f"customers?role=all")
@@ -62,41 +73,121 @@ def email_to_customer_id_and_devices(email):
         print(e)
         return None
 
-def get_cri1_license(email: str):
+def get_userdata_by_email(email):
     try:
-        result = email_to_customer_id_and_devices(email)
-
-        if result:
-            user_membership = get_user_membership(result["customer_id"])
-            user_membership = user_membership[0]
-            
-            if user_membership["status"] == "active":
-                return {
-                    "customer_id": result["customer_id"],
-                    "user_first_name": result["first_name"],
-                    "user_last_name": result["last_name"],
-                    "company_name": result["company_name"],
-                    "max_devices": result["max_devices"],
-                    "valid_license":  True
+        query = """
+            query Query($search: String!) {
+            users(where: {search: $search, searchColumns: EMAIL}) {
+                nodes {
+                    databaseId
+                    firstName
+                    lastName
                 }
+            }
+        }    
+        """
+        variables = {"search": email}
+        data = fetch_graphql(query=query, variables=variables)        
+        
+        return {
+            "id": data["data"]["users"]["nodes"][0]["databaseId"],
+            "first_name": data["data"]["users"]["nodes"][0]["firstName"],
+            "last_name": data["data"]["users"]["nodes"][0]["lastName"],
+        }
+    except Exception as e:
+        print(e)
+        return None
+    
+def get_membership(user_data):
+    try:
+        user_id = user_data["id"]
+        membership_data = fetch_json(f"memberships/members?customer={user_id}")
+        
+        return {
+                    "customer_id": user_id,
+                    "first_name": user_data["first_name"],
+                    "last_name": user_data["last_name"],
+                    "company_name": "",
+                    "status": membership_data[0]["status"],
+                    "max_devices": int(list(filter(lambda item: item["slug"] == "sesiones-activas", membership_data[0]["profile_fields"]))[0]["value"])
+                }
+    except Exception as e:
+        print(e)
+        return None
+
+def main_email_to_customer_id_and_devices(email):
+    try:
+        user_data = get_userdata_by_email(email)
+        
+        if user_data:
+            user_membership = get_membership(user_data)
+
+            if user_membership:
+                return user_membership
+    except Exception as e:
+        print(e)
+    
+    return None
+
+
+def get_cri1_license(email: str):
+    try:        
+        main_user_membership = main_email_to_customer_id_and_devices(email)
+        
+        if main_user_membership is not None:
+            if main_user_membership["status"] == "active":
+                return {
+                        "customer_id": main_user_membership["customer_id"],
+                        "user_first_name": main_user_membership["first_name"],
+                        "user_last_name": main_user_membership["last_name"],
+                        "company_name": main_user_membership["company_name"],
+                        "max_devices": main_user_membership["max_devices"],
+                        "valid_license":  True
+                    }
+            else:
+                    return {
+                        "customer_id": "",
+                        "user_first_name": "",
+                        "user_last_name": "",
+                        "company_name": "",
+                        "max_devices": 0,
+                        "valid_license":  False
+                    }
+            
+        else:
+            result = email_to_customer_id_and_devices(email)
+
+            if result:
+                user_membership = get_user_membership(result["customer_id"])
+                user_membership = user_membership[0]
+                
+                if user_membership["status"] == "active":
+                    return {
+                        "customer_id": result["customer_id"],
+                        "user_first_name": result["first_name"],
+                        "user_last_name": result["last_name"],
+                        "company_name": result["company_name"],
+                        "max_devices": result["max_devices"],
+                        "valid_license":  True
+                    }
+                else:
+                    return {
+                        "customer_id": "",
+                        "user_first_name": "",
+                        "user_last_name": "",
+                        "company_name": "",
+                        "max_devices": 0,
+                        "valid_license":  False
+                    }
             else:
                 return {
-                    "customer_id": "",
-                    "user_first_name": "",
-                    "user_last_name": "",
-                    "company_name": "",
-                    "max_devices": 0,
-                    "valid_license":  False
+                        "customer_id": "",
+                        "user_first_name": "",
+                        "user_last_name": "",
+                        "company_name": "",
+                        "max_devices": 0,
+                        "valid_license":  False
                 }
-        else:
-            return {
-                    "customer_id": "",
-                    "user_first_name": "",
-                    "user_last_name": "",
-                    "company_name": "",
-                    "max_devices": 0,
-                    "valid_license":  False
-            }
     except Exception as e:
         print(e)
         return {
